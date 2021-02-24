00ace51fc883a17525fd53cac1aa7d2db9a9b613
Hyyy_1 <447542807@qq.com>
2021-02-24 09:04:04 +0800

    将标签由字符串变成了一个对象，里面是name和id

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Layout>
+      我是EditLabel
+    </Layout>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+@Component
+export default class EditLabel extends Vue {}
+</script>
+
+<style scoped></style>
