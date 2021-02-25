76eb27737d18b3163fcf35ba8928a45b1371a2de
Hyyy_1 <447542807@qq.com>
2021-02-25 11:23:34 +0800

    模块化的store 删除了models

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,34 +1,8 @@
-import tagListModel from '@/models/tagListModel'
-import recordListModel from '@/models/recordListModel'
+import recordStore from '@/store/recordStore'
+import tagStore from '@/store/tagStore'
+
 const store = {
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => {
-    return recordListModel.create(record)
-  },
-  // tag store
-  tagList: tagListModel.fetch(),
-  // 增
-  createTag: (name: string) => {
-    if (name) {
-      const message = tagListModel.create(name)
-      if (message === 'duplicated') {
-        alert('标签名重复')
-      } else if (message === 'success') {
-        alert('添加成功')
-      }
-    }
-  },
-  // 删
-  removeTag: (id: string) => {
-    return tagListModel.remove(id)
-  },
-  // 改
-  updateTag: (id: string, name: string) => {
-    return tagListModel.update(id, name)
-  },
-  // 查
-  findTag: (id: string) => {
-    return store.tagList.filter((t) => t.id === id)[0]
-  },
+  ...recordStore,
+  ...tagStore,
 }
 export default store
